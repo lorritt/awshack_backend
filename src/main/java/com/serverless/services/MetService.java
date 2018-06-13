3cbbfdfbce66cb4ccc614ebe719e438aefe3c6f7
JSlapp <jordan.slapp@deepskybluesolutions.com>
2018-06-13 10:26:08 +0100

    Updated met service to use key

--- src/main/java/com/serverless/services/MetService.java
+++ src/main/java/com/serverless/services/MetService.java
@@ -17,12 +17,11 @@ public class MetService {
         StringBuffer content = null;
 
         try {
-            //TODO make this work jordon
-//            url = new URL("https://pwms.datapoint.metoffice.gov.uk/points/v1/pwms-hourly-spot-forecast?latitude=" + lat + "&longitude=" + lon);
+            url = new URL("https://pwms.datapoint.metoffice.gov.uk/points/v1/pwms-hourly-spot-forecast?latitude=" + lat + "&longitude=" + lon);
 
-            url = new URL("https://pwms.datapoint.metoffice.gov.uk/points/v1/pwms-hourly-spot-forecast?latitude=51.517187&longitude=-0.104444");
             HttpURLConnection con = (HttpURLConnection) url.openConnection();
-            con.setRequestProperty("api-key",System.getenv("metOfficeAPIKey"));
+            String x = System.getenv("MET_API_KEY");
+            con.setRequestProperty("api-key", System.getenv("MET_API_KEY"));
             con.setRequestMethod("GET");
 
             BufferedReader in = new BufferedReader(
