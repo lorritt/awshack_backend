3c44b555245d8cb1824c2244ba0569246688fb30
deepSkyLee <lee.orritt@deepskybluesolutions.com>
2018-06-13 09:14:01 +0100

    added some met office request logic, but this needs headers adding to function

--- src/main/java/com/serverless/Handler.java
+++ src/main/java/com/serverless/Handler.java
@@ -26,8 +26,22 @@ public class Handler implements RequestHandler<Map<String, Object>, ApiGatewayRe
 	public ApiGatewayResponse handleRequest(Map<String, Object> input, Context context) {
 		LOG.info("received: " + input);
 
-        URL url = null;
-        StringBuffer content = null;
+        Response responseBody = new Response(getTFLAirQuality());
+        Map<String, String> headers = new HashMap<>();
+        headers.put("X-Powered-By", "AWS Lambda & Serverless");
+        headers.put("Content-Type", "application/json");
+
+        return ApiGatewayResponse.builder()
+                .setStatusCode(200)
+                .setObjectBody(responseBody)
+                .setHeaders(headers)
+                .build();
+	}
+
+
+	private String getTFLAirQuality(){
+        URL url;
+        StringBuffer content;
 
         try {
             url = new URL("https://api.tfl.gov.uk/AirQuality?app_id=" + System.getenv("TFL_API_ID") + "&app_key="+ System.getenv("TFL_API_KEY"));
@@ -45,6 +59,7 @@ public class Handler implements RequestHandler<Map<String, Object>, ApiGatewayRe
             in.close();
             con.disconnect();
 
+            return content.toString();
 
         } catch (MalformedURLException e) {
             e.printStackTrace();
@@ -52,17 +67,43 @@ public class Handler implements RequestHandler<Map<String, Object>, ApiGatewayRe
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
+        } finally {
+            return "Could not obtain Air Quality data";
         }
+    }
 
-        Response responseBody = new Response(content.toString());
-        Map<String, String> headers = new HashMap<>();
-        headers.put("X-Powered-By", "AWS Lambda & Serverless");
-        headers.put("Content-Type", "application/json");
+    private String getMetOfficeHourlyForcast(String lat, String lon){
 
-        return ApiGatewayResponse.builder()
-                .setStatusCode(200)
-                .setObjectBody(responseBody)
-                .setHeaders(headers)
-                .build();
-	}
+        URL url;
+        StringBuffer content = null;
+
+        try {
+            //TODO make this work jordon
+            url = new URL("https://pwms.datapoint.metoffice.gov.uk/points/v1/pwms-hourly-spot-forecast?latitude=" + lat + "&longitude=" + lon);
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("GET");
+
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(con.getInputStream()));
+            String inputLine;
+            content = new StringBuffer();
+            while ((inputLine = in.readLine()) != null) {
+                content.append(inputLine);
+            }
+
+            in.close();
+            con.disconnect();
+
+            return content.toString();
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (ProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            return "Could not obtain Hourly Forecast";
+        }
+    }
 }
